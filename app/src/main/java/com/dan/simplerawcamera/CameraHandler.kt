4daac025611364b4faa3637a9b6adcf7c47ec10f
Dan Oprea <dan.oprea@masternaut.com>
2020-12-08 15:52:50 +0100

    First camera view

--- app/src/main/java/com/dan/simplerawcamera/CameraHandler.kt
+++ app/src/main/java/com/dan/simplerawcamera/CameraHandler.kt
@@ -3,6 +3,8 @@ package com.dan.simplerawcamera
 import android.graphics.Rect
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
+import android.hardware.camera2.params.RecommendedStreamConfigurationMap
+import android.hardware.camera2.params.StreamConfigurationMap
 import android.util.Range
 
 class CameraHandler(
@@ -15,65 +17,78 @@ class CameraHandler(
     val exposureCompensantionRange: Range<Int>,
     val focusRange: Range<Float>,
     val focusHyperfocalDistance: Float,
-    val hasFlash: Boolean
+    val hasFlash: Boolean,
+    val sensorOrientation: Int,
+    val streamConfigurationMap: StreamConfigurationMap
 ) {
 
     companion object {
         fun getValidCameras(cameraManager: CameraManager): ArrayList<CameraHandler> {
             val validCameras = ArrayList<CameraHandler>()
 
-            val cameraIds = cameraManager.cameraIdList
+            try {
+                val cameraIds = cameraManager.cameraIdList
 
-            for (cameraId in cameraIds) {
-                val characteristics = cameraManager.getCameraCharacteristics(cameraId)
-                val keys = characteristics.keys
+                for (cameraId in cameraIds) {
 
-                //camera must be minimum level 3
-                if (CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL !in keys) continue
-                if ((characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) as Int) < CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_3) continue
-                if (CameraCharacteristics.CONTROL_AE_COMPENSATION_RANGE !in keys) continue
-                if (CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE !in keys) continue
-                if (CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE !in keys) continue
-                if (CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE !in keys) continue
-                if (CameraCharacteristics.LENS_INFO_HYPERFOCAL_DISTANCE !in keys) continue
-                if (CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE !in keys) continue
+                    try {
+                        val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+                        val keys = characteristics.keys
 
-                val isoRange =
-                    characteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE) as Range<Int>
-                val exposureCompensantionRange =
-                    characteristics.get(CameraCharacteristics.CONTROL_AE_COMPENSATION_RANGE) as Range<Int>
-                val speedRange =
-                    characteristics.get(CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE) as Range<Long>
+                        if ((characteristics.get(CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL) as Int) < CameraCharacteristics.INFO_SUPPORTED_HARDWARE_LEVEL_3) continue
 
-                val focusMinDistance = characteristics.get(CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE) as Float
-                val focusHyperfocalDistance =  characteristics.get(CameraCharacteristics.LENS_INFO_HYPERFOCAL_DISTANCE) as Float
-                val focusRange = Range(0f, focusMinDistance)
+                        val isoRange =
+                            characteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE) as Range<Int>
+                        val exposureCompensantionRange =
+                            characteristics.get(CameraCharacteristics.CONTROL_AE_COMPENSATION_RANGE) as Range<Int>
+                        val speedRange =
+                            characteristics.get(CameraCharacteristics.SENSOR_INFO_EXPOSURE_TIME_RANGE) as Range<Long>
 
-                val hasFlash =
-                    if (CameraCharacteristics.FLASH_INFO_AVAILABLE in keys)
-                        characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE) as Boolean
-                    else
-                        false
+                        val focusMinDistance =
+                            characteristics.get(CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE) as Float
+                        val focusHyperfocalDistance =
+                            characteristics.get(CameraCharacteristics.LENS_INFO_HYPERFOCAL_DISTANCE) as Float
+                        val focusRange = Range(0f, focusMinDistance)
 
-                val resolutionRect = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE) as Rect
+                        val hasFlash =
+                            if (CameraCharacteristics.FLASH_INFO_AVAILABLE in keys)
+                                characteristics.get(CameraCharacteristics.FLASH_INFO_AVAILABLE) as Boolean
+                            else
+                                false
 
-                validCameras.add(
-                    CameraHandler(
-                        cameraManager,
-                        cameraId,
-                        resolutionRect.width(),
-                        resolutionRect.height(),
-                        isoRange,
-                        speedRange,
-                        exposureCompensantionRange,
-                        focusRange,
-                        focusHyperfocalDistance,
-                        hasFlash
-                    )
-                )
+                        val resolutionRect =
+                            characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE) as Rect
+
+                        val sensorOrientation =
+                            characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION) as Int
+
+                        val streamConfigurationMap =
+                            characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP) as StreamConfigurationMap
+                        validCameras.add(
+                            CameraHandler(
+                                cameraManager,
+                                cameraId,
+                                resolutionRect.width(),
+                                resolutionRect.height(),
+                                isoRange,
+                                speedRange,
+                                exposureCompensantionRange,
+                                focusRange,
+                                focusHyperfocalDistance,
+                                hasFlash,
+                                sensorOrientation,
+                                streamConfigurationMap
+                            )
+                        )
+                    } catch (e: Exception) {
+                    }
+                }
+            } catch (e: Exception) {
             }
 
             return validCameras
         }
     }
+
+    val areDimensionsSwapped = sensorOrientation == 90 || sensorOrientation == 270
 }
--- app/src/main/java/com/dan/simplerawcamera/MainActivity.kt
+++ app/src/main/java/com/dan/simplerawcamera/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.dan.simplerawcamera
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.ImageFormat
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.os.Handler
+import android.util.Size
+import android.view.SurfaceHolder
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -13,10 +20,13 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.dan.simplerawcamera.databinding.ActivityMainBinding
+import java.lang.Exception
+import kotlin.math.abs
 import kotlin.system.exitProcess
 
 
 class MainActivity : AppCompatActivity() {
+
     companion object {
         val PERMISSIONS = arrayOf(
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -26,14 +36,30 @@ class MainActivity : AppCompatActivity() {
         )
 
         const val REQUEST_PERMISSIONS = 1
+
+        fun getBestResolution( targetWidth: Int, targetHeight: Int, sizes: Array<Size> ): Size {
+            var bestSize = sizes.last()
+            val targetRatio = targetWidth.toFloat() / targetHeight
+
+            for (size in sizes) {
+                if (size.width > targetWidth || size.height > targetHeight) continue
+                val ratio = size.width.toFloat() / size.height
+                if (abs(ratio - targetRatio) < 0.2) {
+                    if (bestSize.width < size.width)
+                        bestSize = size
+                }
+            }
+
+            return bestSize
+        }
     }
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val cameraManager: CameraManager by lazy { getSystemService(Context.CAMERA_SERVICE) as CameraManager }
-    private val cameraList: ArrayList<CameraHandler> by lazy { CameraHandler.getValidCameras(
-        cameraManager
-    ) }
+    private val cameraList: ArrayList<CameraHandler> by lazy { CameraHandler.getValidCameras(cameraManager) }
     private lateinit var cameraHandler: CameraHandler
+    private var cameraDevice: CameraDevice? = null
+    private var cameraCaptureSession: CameraCaptureSession? = null
 
     private var isoValue = 100
     private var isoAuto = false
@@ -42,6 +68,59 @@ class MainActivity : AppCompatActivity() {
     private var speedAuto = false
 
     private var exposureCompensationValue = 0
+
+    private val surfaceHolderCallback = object: SurfaceHolder.Callback {
+        override fun surfaceCreated(p0: SurfaceHolder) {
+            selectCamera(0)
+        }
+
+        override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun surfaceDestroyed(p0: SurfaceHolder) {
+        }
+    }
+
+    val cameraCaptureSessionCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigureFailed(session: CameraCaptureSession) {}
+
+        override fun onConfigured(session: CameraCaptureSession) {
+            val cameraDevice_ = cameraDevice ?: return
+
+            cameraCaptureSession = session
+
+            val previewRequestBuilder =
+                cameraDevice_.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+                    addTarget(binding.surfaceView.holder.surface)
+                }
+
+            session.setRepeatingRequest(
+                previewRequestBuilder.build(),
+                object : CameraCaptureSession.CaptureCallback() {},
+                Handler { true }
+            )
+        }
+    }
+
+    private val cameraStateCallback = object: CameraDevice.StateCallback() {
+        override fun onDisconnected(p0: CameraDevice) {}
+
+        override fun onError(p0: CameraDevice, p1: Int) {}
+
+        override fun onOpened(cameraDevice_: CameraDevice) {
+            cameraDevice = cameraDevice_
+
+            val sizes = cameraHandler.streamConfigurationMap.getOutputSizes(ImageFormat.YUV_420_888)
+            if (null == sizes || 0 == sizes.size) throw Exception("No sizes available")
+            val previewSize = getBestResolution( binding.surfaceView.width, binding.surfaceView.height, sizes )
+
+            val rotatedPreviewWidth = if (cameraHandler.areDimensionsSwapped) previewSize.height else previewSize.width
+            val rotatedPreviewHeight = if (cameraHandler.areDimensionsSwapped) previewSize.width else previewSize.height
+
+            binding.surfaceView.holder.setFixedSize(rotatedPreviewWidth, rotatedPreviewHeight)
+            cameraDevice_.createCaptureSession(mutableListOf(binding.surfaceView.holder.surface), cameraCaptureSessionCallback, Handler { true })
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -70,11 +149,7 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         when (requestCode) {
             REQUEST_PERMISSIONS -> handleRequestPermissions(requestCode, permissions, grantResults)
         }
@@ -86,11 +161,17 @@ class MainActivity : AppCompatActivity() {
         exitProcess(0)
     }
 
-    private fun handleRequestPermissions(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
+    private fun fatalError(msg: String) {
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.app_name))
+            .setMessage(msg)
+            .setIcon(android.R.drawable.stat_notify_error)
+            .setCancelable(false)
+            .setPositiveButton("OK") { _, _ -> exitApp() }
+            .show()
+    }
+
+    private fun handleRequestPermissions(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         var allowedAll = grantResults.size >= PERMISSIONS.size
 
         if (grantResults.size >= PERMISSIONS.size) {
@@ -105,20 +186,13 @@ class MainActivity : AppCompatActivity() {
         if( allowedAll ) {
             onPermissionsAllowed()
         } else {
-            exitApp()
+            fatalError("Permissions are mandatory !")
         }
     }
 
     private fun onPermissionsAllowed() {
         if (cameraList.size <= 0) {
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.app_name))
-                .setMessage("No valid camara found !")
-                .setIcon(android.R.drawable.stat_notify_error)
-                .setCancelable(false)
-                .setPositiveButton("OK") { _, _ -> exitApp() }
-                .show()
-
+            fatalError("No valid camera found !")
             return
         }
 
@@ -137,9 +211,10 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        selectCamera(0)
+        binding.surfaceView.holder.addCallback( surfaceHolderCallback )
     }
 
+    @SuppressLint("MissingPermission")
     private fun selectCamera(index: Int) {
         cameraHandler = cameraList[index]
 
@@ -147,27 +222,13 @@ class MainActivity : AppCompatActivity() {
 
         val set = ConstraintSet()
         set.clone(binding.layoutView)
-        set.setDimensionRatio(binding.surfaceView.getId(), "${cameraHandler.resolutionWidth}:${cameraHandler.resolutionHeight}")
+        set.setDimensionRatio(
+            binding.frameView.getId(),
+            "${cameraHandler.resolutionWidth}:${cameraHandler.resolutionHeight}"
+        )
         set.applyTo(binding.layoutView)
 
-        /*
-        cameraManager.openCamera(
-            cameraHandler.id,
-            object: CameraDevice.StateCallback() {
-                override fun onDisconnected(p0: CameraDevice) {}
-                override fun onError(p0: CameraDevice, p1: Int) {}
-                override fun onOpened(cameraDevice: CameraDevice) {
-                    val cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraDevice.id)
-                    cameraCharacteristics[CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP]?.let { streamConfigurationMap ->
-                        streamConfigurationMap.getOutputSizes(ImageFormat.YUV_420_888)?.let { yuvSizes ->
-                            val previewSize = yuvSizes.last()
-                        }
-                    }
-                }
-            },
-            Handler { true })
-
-         */
+        cameraManager.openCamera(cameraHandler.id, cameraStateCallback, Handler { true } )
     }
 
     private fun askPermissions(): Boolean {
