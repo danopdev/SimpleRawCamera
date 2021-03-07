64ccfa8ba0b50ae06730fe10ee4626df6cb68a00
dan <doprea@gmail.com>
2021-03-07 19:38:03 +0100

    Tune Protect highlights high lights

--- app/src/main/java/com/dan/simplerawcamera/Settings.kt
+++ app/src/main/java/com/dan/simplerawcamera/Settings.kt
@@ -16,7 +16,7 @@ class Settings( private val activity: Activity) {
 
         const val PROTECT_HIGHLIGHTS_MAX_SPEED = 125000000L // 1/8 sec
         const val PROTECT_HIGHLIGHTS_OVER_EXPOSED_THRESHOLD = 1 //pixels in the histogram
-        const val PROTECT_HIGHLIGHTS_OVER_EXPOSED_START_INDEX = 64
+        const val PROTECT_HIGHLIGHTS_OVER_EXPOSED_START_INDEX = 63
         const val PROTECT_HIGHLIGHTS_UNDER_EXPOSED_THRESHOLD = 2 //pixels in the histogram
         const val PROTECT_HIGHLIGHTS_UNDER_EXPOSED_START_INDEX = 45
 
